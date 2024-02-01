d57e8fec240bc51d64683b0a3bf7e174c2a04a15
himanoa <matsunoappy@gmail.com>
2024-02-01 22:48:52 +0900

    chore(package.json): add "@types/node" as a dev dependency to enable type checking for Node.js modules
    feat(cli.ts): add support for defining commands and running the main program using the "citty" library
    
    feat(commands/inspect.ts): add a new command "inspect" to the CLI that logs "inspect" to the console
    
    chore(tsconfig.json): enable resolving of .json files and set module resolution to "node10" for better compatibility with Node.js modules

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,14 @@
-console.log("hello")
+import { defineCommand, runMain } from "citty";
+
+const program = defineCommand({
+  meta: {
+    name: process.env.npm_package_name,
+    version: process.env.npm_package_version,
+    description: process.env.package_description,
+  },
+  subCommands: {
+    inspect: () => import('./commands/inspect').then(r => r.default)
+  },
+})
+
+runMain(program)
---
+++ src/commands/inspect.ts
@@ -0,0 +1,11 @@
+import { defineCommand } from "citty";
+
+export default defineCommand({
+  meta: {
+    name: 'inspect',
+    description: "Inspect the package dependencies"
+  },
+  run: () => {
+    console.log("inspect")
+  }
+})
