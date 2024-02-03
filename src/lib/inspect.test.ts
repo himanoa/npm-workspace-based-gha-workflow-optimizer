ae01062befc4f6f15acbc8dc497ed8ac53ce83a2
himanoa <matsunoappy@gmail.com>
2024-02-03 12:27:18 +0900

    Fix failed inspect test

--- src/lib/inspect.test.ts
+++ src/lib/inspect.test.ts
@@ -1,11 +1,14 @@
 import { describe, it, expect, vi } from "vitest";
 import { makeInspect } from "./inspect";
 import { Ok } from "ts-results";
+import { BiMap } from "@rimbu/core";
 
 describe("inspect", () => {
   describe("if workspace only mode", () => {
     it("called makeDependenciesGraph function", async () => {
-      const makeDependenciesGraph = vi.fn();
+      const makeDependenciesGraph = vi.fn(async () =>
+        Ok({ graph: [], idToValue: BiMap.empty<number, string>() }),
+      );
       const inspect = makeInspect({
         makeDependenciesGraph: makeDependenciesGraph,
         readRootPackage: async () =>
@@ -19,7 +22,9 @@ describe("inspect", () => {
     });
 
     it("called readRootPackage function", async () => {
-      const makeDependenciesGraph = vi.fn();
+      const makeDependenciesGraph = vi.fn(async () =>
+        Ok({ graph: [], idToValue: BiMap.empty<number, string>() }),
+      );
       const readRootPackage = vi.fn(async () =>
         Ok({
           name: "root",
