171ea2c9e7bbb19e5267f85a19a410c1ab8a6303
himanoa <matsunoappy@gmail.com>
2024-02-05 12:59:06 +0900

    refactor(inspect.ts): improve code formatting and indentation for better readability

--- src/lib/inspect.ts
+++ src/lib/inspect.ts
@@ -7,22 +7,21 @@ type Dependencies = {
   readRootPackage: ReadRootPackage;
 };
 
-export const makeInspect =
-  (deps: Dependencies) => async () => {
-      const rootPackageResult = await deps.readRootPackage();
-      const rootPackage = await match(rootPackageResult)
-        .with({ ok: true }, async ({ val: rootPackage }) => {
-          return rootPackage;
-        })
-        .with({ err: true }, async () => {
-          console.error("Not found package.json");
-          process.exit(1);
-        })
-        .exhaustive();
+export const makeInspect = (deps: Dependencies) => async () => {
+  const rootPackageResult = await deps.readRootPackage();
+  const rootPackage = await match(rootPackageResult)
+    .with({ ok: true }, async ({ val: rootPackage }) => {
+      return rootPackage;
+    })
+    .with({ err: true }, async () => {
+      console.error("Not found package.json");
+      process.exit(1);
+    })
+    .exhaustive();
 
-      const graphResult = await deps.makeDependenciesGraph(rootPackage);
-      graphResult.map((graph) => {
-        console.dir(graph);
-      });
-      return;
-  };
+  const graphResult = await deps.makeDependenciesGraph(rootPackage);
+  graphResult.map((graph) => {
+    console.dir(graph);
+  });
+  return;
+};
