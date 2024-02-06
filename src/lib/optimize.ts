6e7970803cc9a3ecae1f6d0b56162f05e11a85bc
himanoa <matsunoappy@gmail.com>
2024-02-06 23:23:01 +0900

    refactor(optimize.ts): remove commented out code and add missing dependencies parameter to makeOptimize function
    The commented out code in the makeOptimize function was removed to improve code readability and maintainability. Additionally, the missing dependencies parameter was added to the makeOptimize function to ensure that the required dependencies are properly injected.

--- src/lib/optimize.ts
+++ src/lib/optimize.ts
@@ -1,3 +1,4 @@
+import { match } from "ts-pattern";
 import { MakeDependenciesGraph } from "./dependencies/make-dependencies-graph"
 import { ReadRootPackage } from "./npm/read-root-package"
 
@@ -6,18 +7,19 @@ type Dependencies = {
   makeDependenciesGraph: MakeDependenciesGraph
 }
 
-export const makeOptimize = () => async () => {
-  // const rootPackageResult = await deps.readRootPackage();
-  // const rootPackage = await match(rootPackageResult)
-  //   .with({ ok: true }, async ({ val: rootPackage }) => {
-  //     return rootPackage;
-  //   })
-  //   .with({ err: true }, async () => {
-  //     console.error("Not found package.json");
-  //     process.exit(1);
-  //   })
-  //   .exhaustive();
+export const makeOptimize = (deps: Dependencies) => async () => {
+  const rootPackageResult = await deps.readRootPackage();
+  const rootPackage = await match(rootPackageResult)
+    .with({ ok: true }, async ({ val: rootPackage }) => {
+      return rootPackage;
+    })
+    .with({ err: true }, async () => {
+      console.error("Not found package.json");
+      process.exit(1);
+    })
+    .exhaustive();
 
-  // const _graphResult = await deps.makeDependenciesGraph(rootPackage);
+  const graphResult = await deps.makeDependenciesGraph(rootPackage);
+  console.dir(graphResult)
   return;
 };
