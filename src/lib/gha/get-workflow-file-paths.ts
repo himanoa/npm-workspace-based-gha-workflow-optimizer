13b0a7048bbb3c0231b94143fbeba579b22eed9d
himanoa <matsunoappy@gmail.com>
2024-02-19 10:43:53 +0900

    Add support for scoped package

--- src/lib/gha/get-workflow-file-paths.ts
+++ src/lib/gha/get-workflow-file-paths.ts
@@ -1,5 +1,9 @@
 import { glob } from "glob";
 
 export const getWorkflowFilePaths = async (packageName: string) => {
-  return glob(`.github/workflows/${packageName}-*.yml`);
+  return glob(`.github/workflows/${normalizePackageName(packageName)}-*.yml`);
 };
+
+export const normalizePackageName = (packageName: string): string => {
+  return packageName.replace("@", "").replace('/', '-')
+}
